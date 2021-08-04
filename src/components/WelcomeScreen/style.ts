a56f614610f4058fe2080161f6f308b7da8673c1
Valter <valteralvees@outlook.com>
2021-08-04 17:33:37 -0300

    Update style.ts

--- src/components/WelcomeScreen/style.ts
+++ src/components/WelcomeScreen/style.ts
@@ -2,8 +2,19 @@ import styled from 'styled-components'
 
 export const Header = styled.header`
   max-width: 1500px;
+  margin: 0 auto;
+
+  padding: 0 1rem 10rem;
+  display: flex;
+  align-items:center;
+  justify-content: space-between;
 
 `;
 export const Content = styled.div`
+  max-width:1200px;
+  margin: 0 auto;
+  display: block;
+  align-items: center;
+  
 
 `;
